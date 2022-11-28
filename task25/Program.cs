b6be4fe44f28fc90996d9d170a54b90872a012d9
RomanKolegov <kolegov_rv@mail.ru>
2022-11-28 15:20:42 +0500

    add task25

---
+++ task25/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+int extentOfNumbers(int numberA, int numberB)
+{
+    int mult = 1;
+    for (int i = 1; i <= numberB; i++)
+    {
+        mult = mult * numberA;
+    }
+    return mult;
+}
+int getNumberFromUser(string message)
+{
+    int result = 0;
+    Console.WriteLine(message);
+    result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+int numberA = getNumberFromUser("Введите число A");
+int numberB = getNumberFromUser("Введите число B");
+int extent = extentOfNumbers(numberA, numberB);
+Console.WriteLine($"При возведении числа {numberA} в натураотную степень из числа {numberB} получаем результат {extent}");
